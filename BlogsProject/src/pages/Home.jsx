400027590d9b2b296763ed19368cb7cb5699d6b2
Naik Mubashir <naikmubashir095@gmail.com>
2024-09-24 20:22:58 +0530

    Add Home page

---
+++ BlogsProject/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import appwriteService from "../appwrite/config";
+import { Container, PostCard } from "../components";
+
+const Home = () => {
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    appwriteService.getPosts().then((postsData) => {
+      if (postsData) {
+        setPosts(postsData.documents);
+      }
+    });
+  }, []);
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                Login to read posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
