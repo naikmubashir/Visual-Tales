5e8094f3f1cd6a18ca6ebb282d38286007e5f787
Naik Mubashir <naikmubashir095@gmail.com>
2024-09-24 20:21:34 +0530

    Add EditPost page

---
+++ BlogsProject/src/pages/EditPost.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react'
+import {Container, PostForm} from '../components'
+import appwriteService from "../appwrite/config";
+import { useNavigate,  useParams } from 'react-router-dom';
+
+const EditPost = () => {
+    const [post,setPosts]=useState(null);
+    const {slug}=useParams();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(post){
+          appwriteService.getPost(slug).then((post)=>{
+            if(post)setPosts(post);
+          })  
+        }else{
+            navigate('/')
+        }
+    },[slug, navigate])
+
+  return post?(
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ):null
+}
+
+export default EditPost
