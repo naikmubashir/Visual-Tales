1e677ef54f3edc8d217d3242d964c5fb7483f15d
Naik Mubashir <naikmubashir095@gmail.com>
2024-09-24 16:06:25 +0530

    Add PostForm.jsx

--- BlogsProject/src/components/index.js
+++ BlogsProject/src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button";
+import Input from "./Input";
+import AuthLayout from "./AuthLayout";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
 
 
-export{Header, Footer, Container, Logo, LogoutBtn}
+export{Header, Footer, Container, Logo, LogoutBtn, Button,Input, AuthLayout, RTE, PostForm}
