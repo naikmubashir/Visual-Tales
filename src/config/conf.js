0392a0b21da0a465ef99ffba9c13fc0d76d75493
Naik Mubashir <124430100+naikmubashir@users.noreply.github.com>
2025-01-01 21:55:12 +0530

    Update conf.js

--- src/config/conf.js
+++ src/config/conf.js
@@ -4,7 +4,7 @@ const conf ={
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    apiKey: String(import.meta.env.VITE_APPWRITE_API_KEY)
+    apiKey: String(import.meta.env.VITE_API_KEY)
 }
 
 export default conf
